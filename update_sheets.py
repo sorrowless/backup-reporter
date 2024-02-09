1361635afd79e5959ca6e2373af4ac31a5df1c0b
Andrey Bogdanov <a.bogdanov@gmail.com>
2024-02-09 16:46:45 +0000

    Update file update_sheets.py

--- update_sheets.py
+++ update_sheets.py
@@ -6,13 +6,12 @@ import json
 import os
 
 project_name = os.environ.get("PROJECT_NAME", "test_project")
-# clietn_name = os.environ.get("CLIENT_NAME")
+clietn_name = os.environ.get("CLIENT_NAME", "test_client")
 spreadsheet_name = os.environ.get("SPREADSHEET_NAME", "Backup_sheet")
 sheet_name = os.environ.get("SHEET_NAME", "Test_sheet")
 print(sheet_name)
 # Replace these variables with your own values
 json_file_path = './credantionals.json'
-# spreadsheet_name = 'Backup_sheet'
 json_data_reports_file = './backup_report.json'
 json_data_validate_file = './validate_report.json'
 
@@ -35,9 +34,9 @@ with open(json_data_reports_file, 'r') as json_file:
 with open(json_data_validate_file, 'r') as json_file:
     data_validation = json.load(json_file)
 
-full_update_data = [project_name, datetime.now().strftime("%d_%m_%Y-%H_%M_%S")] + list(data_reports.values()) + list(data_validation.values())
+full_update_data = [clietn_name, project_name, datetime.now().strftime("%d_%m_%Y-%H_%M_%S")] + list(data_reports.values()) + list(data_validation.values())
 
-target_cell = sheet.find(project_name, in_column=1)
+target_cell = sheet.find(project_name, in_column=2)
 print(target_cell)
 
 if target_cell != None:
