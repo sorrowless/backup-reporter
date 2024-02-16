c97b2fa6e6ac9ad713ae5da4a39464f5bd878096
d.razin <drazin@oom.ag>
2024-02-16 17:27:54 +0300

    Some fixes

--- dataclass.py
+++ dataclass.py
@@ -9,10 +9,12 @@ class BackupMetadata:
     type: str = None
     size: str = None
     time: str = None
+    customer: str = None
     placement: str = None
     backup_name: str = None
     last_backup_date: str = None
     count_of_backups: str = None
+    supposed_backups_count: str = None
 
     def __str__(self):
         '''String representation of that DataClass is valid json string'''
