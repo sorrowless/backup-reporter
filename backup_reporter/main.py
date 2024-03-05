a62605e3f292b391f1c1bbea7a1dd22443f3a07e
d.razin <drazin@oom.ag>
2024-03-05 15:21:19 +0300

    delete useless cli args

--- backup_reporter/main.py
+++ backup_reporter/main.py
@@ -10,95 +10,11 @@ from backup_reporter.utils import set_confs
 
 def start():
     arg_parser = argparse.ArgumentParser()
-    execution_mode = arg_parser.add_mutually_exclusive_group()
 
     arg_parser.add_argument("--config",
         type=str,
         default="",
         help="Set config path"
-    )
-
-    is_config_set = any('--config' in arg for arg in sys.argv)
-
-    arg_parser.add_argument("--logging_level",
-        type=str,
-        default="INFO",
-        help="Python loggin facility level. Possible values: NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL"
-    )
-
-    arg_parser.add_argument("--bucket",
-        type=ast.literal_eval,
-        default=[],
-        required=not is_config_set,
-        action='append',
-        help="""The bucket from which metadata will be taken.
-        You can pass that any --bucket arguments.
-        Format MUST be like following: --bucket={'s3_path': 's3://bucket_name/in_bucket_path/metadata_file_name.txt', 'aws_access_key_id': 'key', 'aws_secret_access_key': 'key', 'aws_region': 'region'}"""
-    )
-
-    arg_parser.add_argument("--customer",
-        type=str,
-        default="",
-        required=not is_config_set,
-        help="Backup customer"
-    )
-
-    arg_parser.add_argument("--supposed_backups_count",
-        type=str,
-        default="",
-        required=not is_config_set,
-        help="Supposed backups count"
-    )
-
-    ### Reported specific args
-
-    execution_mode.add_argument("--docker_postgres",
-        default=False,
-        action='store_true',
-        help="Setup reporter for dockerized postgres"
-    )
-
-    arg_parser.add_argument("--container_name",
-        type=str,
-        default="",
-        required=not is_config_set and '--docker_postgres' in sys.argv,
-        help="Set container name for databases inside docker containers"
-    )
-
-    ### Collector mode args
-
-    execution_mode.add_argument("--collector",
-        default=False,
-        action='store_true',
-        help="Setup collector"
-    )
-
-    arg_parser.add_argument("--google_spreadsheet_credentials_path",
-        type=str,
-        default="",
-        required=not is_config_set and '--collector' in sys.argv,
-        help="""Path to credentials json to connect to google sheet"""
-    )
-
-    arg_parser.add_argument("--spreadsheet_name",
-        type=str,
-        default="",
-        required=not is_config_set and '--collector' in sys.argv,
-        help="""Spreadsheet name where backups info will store"""
-    )
-
-    arg_parser.add_argument("--worksheet_name",
-        type=str,
-        default="",
-        required=not is_config_set and '--collector' in sys.argv,
-        help="""Worksheet name where backups info will store"""
-    )
-
-    arg_parser.add_argument("--sheet_owner",
-        type=str,
-        default="",
-        required=not is_config_set and '--collector' in sys.argv,
-        help="""Spreadsheet owner to share with"""
     )
 
     arguments = arg_parser.parse_known_args()[0]
